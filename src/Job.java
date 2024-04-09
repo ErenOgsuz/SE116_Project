c0f566df4a85addc835498572a6401520e80ec32
ErenOg <erenogsuz@hotmail.com>
2024-04-09 12:28:05 +0300

    Classes Rearranged
    Deleted some unuseful variables

--- src/Job.java
+++ src/Job.java
@@ -6,19 +6,23 @@ public class Job {
     private int duration;
     private ArrayList<Task> tasks;
     private int taskIndex;
-    private Station station;
     private int deadline;
-    private String status;
 
-    public Job(String jobID, int startTime, int duration, ArrayList<Task> tasks, int taskIndex, Station station, int deadline, String status) {
+    public Job(String jobID, int startTime, ArrayList<Task> tasks, int taskIndex, int deadline) {
         this.jobID = jobID;
         this.startTime = startTime;
-        this.duration = duration;
         this.tasks = tasks;
         this.taskIndex = taskIndex;
-        this.station = station;
         this.deadline = deadline;
-        this.status = status;
     }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
     public Job(){}
 }
--- src/Task.java
+++ src/Task.java
@@ -2,18 +2,11 @@ public class Task {
     private String taskTypeID;
     private double size;
     private Station station;
-    private Job jobType;
-    private int startTime;
-    private int endTime;
 
     public Task(String taskTypeID){
         this.taskTypeID=taskTypeID;
         this.size=0.0;
-        this.startTime=0;
-        this.endTime=0;
         this.station=new Station();
-        this.jobType=new Job();
-
     }
 
     public String getTaskTypeID() {
@@ -38,29 +31,5 @@ public class Task {
 
     public void setStation(Station station) {
         this.station = station;
-    }
-
-    public Job getJobType() {
-        return jobType;
-    }
-
-    public void setJobType(Job jobType) {
-        this.jobType = jobType;
-    }
-
-    public int getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(int startTime) {
-        this.startTime = startTime;
-    }
-
-    public int getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(int endTime) {
-        this.endTime = endTime;
     }
 }
