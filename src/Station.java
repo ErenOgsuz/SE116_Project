6b0f74668a58b6faeec12962e18fa5939d08b540
Sezen Çetinkaya <152431280+SezenCetinkaya@users.noreply.github.com>
2024-04-23 14:21:34 +0300

    Update Station.java

--- src/Station.java
+++ src/Station.java
@@ -16,8 +16,66 @@ public class Station {
         this.multiFlag = multiFlag;
         this.tasks = tasks;
     }
+    public Station(int stationID, ArrayList<Task> tasks){
+        this.stationID = stationID;
+        this.tasks = tasks;
+    }
 
-    public Station(){
+    public Station(){}
 
+    public int getStationID() {
+        return stationID;
+    }
+
+    public void setStationID(int stationID) {
+        this.stationID = stationID;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public boolean isMultiFlag() {
+        return multiFlag;
+    }
+
+    public void setMultiFlag(boolean multiFlag) {
+        this.multiFlag = multiFlag;
+    }
+
+    public boolean isFifoFlag() {
+        return fifoFlag;
+    }
+
+    public void setFifoFlag(boolean fifoFlag) {
+        this.fifoFlag = fifoFlag;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
     }
 }
